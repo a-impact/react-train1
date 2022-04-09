9a393fef8c1c3c359ab002ce3ca40e0e992132c0
a_tokuda555 <a_tokuda@enfactory.co.jp>
2022-04-10 02:00:15 +0900

    propsについて

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Article from './components/Article';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Article
+        title={"タイトルタイトル"}
+        content={"コンテントコンテント"}
+      />
+      <Article
+        title={"何でやねん"}
+        content={"お好み焼き"}
+      />
+      <Article
+        title={"イエスイエス"}
+        content={"わははっはは"}
+      />
     </div>
   );
 }
